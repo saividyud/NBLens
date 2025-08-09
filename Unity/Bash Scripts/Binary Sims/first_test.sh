29c9f8e8f267c6a16e6d6961f5ec9eeea79a16f6
saividyud <senthilnathan.11@osu.edu>
2025-08-08 21:02:21 -0400

    Testing parallel computations

---
+++ Unity/Bash Scripts/Binary Sims/first_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=4:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem-per-cpu=2G
+#SBATCH --job-name=binary_lens
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=senthilnathan.11@osu.edu
+
+#SBATCH --output="./Unity/Output Logs/Binary_Collection/binary_output.txt"
+
+# Commands to run
+module load mamba
+mamba activate .venv
+python "./Unity/Python Scripts/Binary Sim/binary_binoffset.py" -s1 0.8 -q1 0.001
