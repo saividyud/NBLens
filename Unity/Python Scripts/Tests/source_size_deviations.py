fea0a8bb6aeca405c15a7fa5cffcc2478a590b04
saividyud <senthilnathan.11@osu.edu>
2025-09-23 09:41:21 -0400

    Changes

--- Unity/Python Scripts/Tests/source_size_deviations.py
+++ Unity/Python Scripts/Tests/source_size_deviations.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
     print(f'Shooting single lens:\n')
 
     calculator = IRSC.IRSCaustics(annulus_param_dict=param_dict)
-    magnifications = calculator.series_calculate(cm_offset='auto', annulus_offset=binary_offset, rows=10)
+    magnifications = calculator.series_calculate(cm_offset='auto', rows=10)
 
     print('=========================================================')
 
