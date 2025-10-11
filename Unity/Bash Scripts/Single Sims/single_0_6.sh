77eb1a8360487722859ea29de8f5207a0c2f356e
saividyud <senthilnathan.11@osu.edu>
2025-10-10 23:30:09 -0400

    Fixing holes in single simulations :

---
+++ Unity/Bash Scripts/Single Sims/single_0_6.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=44:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --job-name=single_lens_array_0.6
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=senthilnathan.11@osu.edu
+
+#SBATCH --array=0-55
+
+#SBATCH --output="./Unity/Output Logs/Single_Collection/single_0.6_%a_output.txt"
+
+# Commands to run
+module load mamba
+mamba activate .venv
+
+# Defining parameters
+ss=0.6
+qs=(1e-5 3e-5 1e-4 3e-4 1e-3)
+
+# Compute indices
+s_index=$((SLURM_ARRAY_TASK_ID / 5))
+q_index=$((SLURM_ARRAY_TASK_ID % 5))
+
+# Extract parameters
+s=$ss
+q=${qs[$q_index]}
+
+echo "Running $s separation with $q mass ratio with origin of (0, 0)"
+
+python "./Unity/Python Scripts/Single Sims/single_binoffset.py" -s $s -q $q
