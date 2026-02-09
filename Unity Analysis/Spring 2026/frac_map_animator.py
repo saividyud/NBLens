4893e5159508e78160e8dbb7cd4519e3d1f3c9a9
saividyud <senthilnathan.11@osu.edu>
2026-02-09 09:24:49 -0500

    Plotting

--- Unity Analysis/Spring 2026/frac_map_animator.py
+++ Unity Analysis/Spring 2026/frac_map_animator.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
 
+    import argparse
+
     plt.rcParams['font.family'] = 'Times New Roman'
     plt.rcParams['figure.titlesize'] = 20
     plt.rcParams['figure.titleweight'] = 'bold'
@@ -49,6 +51,12 @@ if __name__ == '__main__':
                 colors.append('red')
         return colors
     
+    #%% Defining arguments
+    parser = argparse.ArgumentParser(description='Plot fractional area maps for microlensing simulations.')
+    parser.add_argument('--multiplier', type=float, default=1.0, help='Multiplier for output filename.')
+    args = parser.parse_args()
+    multiplier = args.multiplier
+    
     #%% Defining parameter space
     ss_str = ['0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0', '1/0.9', '1/0.8', '1/0.7', '1/0.6', '1/0.5', '1/0.4', '1/0.3', '1/0.2']
     ss = np.array([numexpr.evaluate(s) for s in ss_str])
@@ -63,32 +71,32 @@ if __name__ == '__main__':
 
     fig.suptitle('Fractional Area v Source Star Radius ($s \leq 1$)', y=1.0)
 
-    #%% Looping through parameter space
-    def update(frame):
-        multiplier = multipliers[frame]
-        print(f'Processing multiplier: {multiplier:.0e}')
+    # #%% Looping through parameter space
+    # def update(frame):
+    #     multiplier = multipliers[frame]
+    #     print(f'Processing multiplier: {multiplier:.0e}')
 
-        for i, s in enumerate(ss[ss <= 1]):
-            for j, q in enumerate(qs):
-                ax = axes[i, j]
-                ax.set_title(f'$s={ss_str[i]}$, $q={q:.0e}$')
+    for i, s in enumerate(ss[ss <= 1]):
+        for j, q in enumerate(qs):
+            ax = axes[i, j]
+            ax.set_title(f'$s={ss_str[i]}$, $q={q:.0e}$')
 
-                frac_map = np.load(f'./Unity/Simulations/Frac_Maps/Frac_Maps_{multiplier:.0e}/frac_diff_map_q{q:.0e}_s{s:.2e}.npy')
+            frac_map = np.load(f'./Unity/Simulations/Frac_Maps/Frac_Maps_{multiplier:.0e}/Frac_Maps_{q:.0e}/frac_diff_map_q{q:.0e}_s{s:.2e}.npy')
 
-                ang_width = binary_attributes.loc[(binary_attributes['q'] == q) & (binary_attributes['s'] == ss_str[i]), 'ang_width'].values[0]
-                pixels = binary_attributes.loc[(binary_attributes['q'] == q) & (binary_attributes['s'] == ss_str[i]), 'pixels'].values[0]
+            ang_width = binary_attributes.loc[(binary_attributes['q'] == q) & (binary_attributes['s'] == ss_str[i]), 'ang_width'].values[0]
+            pixels = binary_attributes.loc[(binary_attributes['q'] == q) & (binary_attributes['s'] == ss_str[i]), 'pixels'].values[0]
 
-                X_pix, Y_pix = np.meshgrid(np.linspace(-ang_width/2, ang_width/2, pixels), np.linspace(-ang_width/2, ang_width/2, pixels))
+            X_pix, Y_pix = np.meshgrid(np.linspace(-ang_width/2, ang_width/2, pixels), np.linspace(-ang_width/2, ang_width/2, pixels))
 
-                img = ax.contour(
-                    X_pix, Y_pix, np.flip(frac_map, axis=0),
-                    levels=log_array(-3, -1).tolist(),
-                    colors=colors_by_log(log_array(-3, -1)),
-                    linewidths=0.5
-                )
+            img = ax.contour(
+                X_pix, Y_pix, np.flip(frac_map, axis=0),
+                levels=log_array(-3, -1).tolist(),
+                colors=colors_by_log(log_array(-3, -1)),
+                linewidths=0.5
+            )
 
-    ani = animation.FuncAnimation(fig, update, frames=len(multipliers), repeat=False)
-    ani.save('./Unity Analysis/Spring 2026/Figures/frac_map_animation.gif', writer='pillow', fps=1)
+    # ani = animation.FuncAnimation(fig, update, frames=len(multipliers), repeat=False)
+    fig.savefig(f'./Unity Analysis/Spring 2026/Figures/frac_map_plot_{multiplier:.0e}_s<1.png', dpi=300)
 
 
 
--- Unity Analysis/Spring 2026/frac_map_animator.sh
+++ Unity Analysis/Spring 2026/frac_map_animator.sh
@@ -8,10 +8,19 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=senthilnathan.11@osu.edu
 
-#SBATCH --output="./Unity/Output Logs/Analysis/animator_output.txt"
+#SBATCH --array=0-4
+
+#SBATCH --output="./Unity/Output Logs/Analysis/animator_%a_output.txt"
 
 # Commands to run
 module load mamba
 mamba activate .venv
 
-python "./Unity Analysis/Spring 2026/frac_map_animator.py"
+# Defining parameters
+rs=(1e-1 3e-1 1e0 3e0 1e1)
+
+r=${rs[$SLURM_ARRAY_TASK_ID]}
+
+echo "Running animator with radius multiplier of $r"
+
+python "./Unity Analysis/Spring 2026/frac_map_animator.py" --multiplier $r
