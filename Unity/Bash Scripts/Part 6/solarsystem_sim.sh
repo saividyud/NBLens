f3a41de2150e5f5b5f721fd255c2c8e22fbc2c36
saividyud <senthilnathan.11@osu.edu>
2026-06-04 00:33:37 -0400

    Small changes

--- Unity/Bash Scripts/Part 6/solarsystem_sim.sh
+++ Unity/Bash Scripts/Part 6/solarsystem_sim.sh
@@ -21,4 +21,4 @@ export LD_LIBRARY_PATH=$CUDA_HOME/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
 mamba activate .venv
 
 # Run simulation
-python "./Unity/Python Scripts/Part 6/solarsystem_sim.py" --job_id 6
+python "./Unity/Python Scripts/Part 6/solarsystem_sim.py" --job_id 5
