f38c0d82ce3729d40c1f4143ba6a0dbf427d1d3c
saividyud <senthilnathan.11@osu.edu>
2026-06-04 00:35:18 -0400

    Starting many planet simulations

--- Unity/Python Scripts/Part 6/solarsystem_sim.py
+++ Unity/Python Scripts/Part 6/solarsystem_sim.py
@@ -193,7 +193,7 @@ if __name__ == '__main__':
     }
     
     sim_current_lens = IRSC.IRSCaustics(annulus_param_dict=lens_parameters)
-    magnifications_current_lens = sim_current_lens.series_calculate(cm_offset='auto')
+    magnifications_current_lens = sim_current_lens.series_calculate(cm_offset='auto', safety_margin=0.7)
 
     file_name = current_planets.iloc[0]
     for i, planet in enumerate(current_planets.values):
